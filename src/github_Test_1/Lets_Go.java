e08e7ac253ef3c17b0a904344b7ba74f66c6e2f6
MinJoonAhn <mjsky0041@gmail.com>
2020-10-20 10:41:14 +0900

    test2

--- src/github_Test_1/Lets_Go.java
+++ src/github_Test_1/Lets_Go.java
@@ -1,4 +1,4 @@
-package github_Test_1;
+﻿package github_Test_1;
 
 public class Lets_Go {
 
@@ -8,6 +8,7 @@ public class Lets_Go {
 		System.out.println("My name is Changwon!!");
 		//
 		//나는 영재인데 할줄을 잘 몰라서 막 지껄이는중
+		// 집가자 다해간다
 
 	}
 
