ba403b04edd1be3f588ccb1794e015e85bb06caf
MinJoonAhn <mjsky0041@gmail.com>
2020-10-20 10:44:58 +0900

    충돌테스트

--- src/github_Test_1/Lets_Go.java
+++ src/github_Test_1/Lets_Go.java
@@ -3,12 +3,7 @@
 public class Lets_Go {
 
 	public static void main(String[] args) {
-		//Initial Text
-		System.out.println("Hi everyone~");
-		System.out.println("My name is Changwon!!");
-		//
-		//나는 영재인데 할줄을 잘 몰라서 막 지껄이는중
-		// 집가자 다해간다
+		String test1;
 
 	}
 
