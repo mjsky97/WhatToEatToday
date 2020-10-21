601d271c5bd3a1e6166919494f739a5d05ced3cf
MinJoonAhn <mjsky0041@gmail.com>
2020-10-21 15:19:52 +0900

    기능코드 수정(1차-추가)

--- src/main/Keyword.java
+++ src/main/Keyword.java
@@ -6,10 +6,10 @@ public class Keyword {
 	public Fastfood fastfood;
 	public Theme theme;
 	
-	public Liquor_Category_List [] = {"소주", "맥주", "막걸리", "술&밥"}
-	public Situation_Category_List [] = {"회식", "데이트", "과팅(소개팅)", "간단한 식사", "가성비", "비오는날"}
-	public Fastfood_Category_List [] = {"치킨", "햄버거", "피자"}
-	public Theme_Category_List [] = {"국밥", "한식", "중식", "양식", "밀면", "분식"}
+	public String Liquor_Category_List [] = {"소주", "맥주", "막걸리", "술&밥"};
+	public String Situation_Category_List [] = {"회식", "데이트", "과팅(소개팅)", "간단한 식사", "가성비", "비오는날"};
+	public String Fastfood_Category_List [] = {"치킨", "햄버거", "피자"};
+	public String Theme_Category_List [] = {"국밥", "한식", "중식", "양식", "밀면", "분식"};
 	
 	public Keyword(int [] code_list) {
 		liquor = new Liquor(code_list[0]);
